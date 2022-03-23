7ad863e8b08050d8490c3f12922f122b7107cbf3
13725495536 <1808176136qq@.com>
2022-03-24 00:47:56 +0800

    在写订单页了

--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ import store from '@/store'
 //引入路由
 import router from '@/router'
 //按需引入elementui
-import {Row,Col,Menu,Submenu,MenuItem} from 'element-ui'
+import {Row,Col,Menu,Submenu,MenuItem,Table,TableColumn} from 'element-ui'
 Vue.use(Row)
 Vue.use(Col)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
+Vue.use(Table)
+Vue.use(TableColumn)
 
 new Vue({
   render: h => h(App),
--- src/pages/admin/adminHome/index.vue
+++ src/pages/admin/adminHome/index.vue
@@ -3,9 +3,7 @@
     <header>后台管理系统</header>
     <div>
       <el-row class="tac">
-       
         <el-col :span="12">
-         
           <el-menu
             default-active="2"
             class="el-menu-vertical-demo"
@@ -15,27 +13,30 @@
             text-color="#fff"
             active-text-color="#ffd04b"
           >
-           
-            <el-menu-item index="2">
+            <el-menu-item index="2" class="eit">
               <i class="el-icon-menu"></i>
               <span slot="title">商品管理</span>
-              
             </el-menu-item>
-              <el-menu-item index="2">
+            <el-menu-item index="2" class="eit">
               <i class="el-icon-menu"></i>
               <span slot="title">订单管理</span>
             </el-menu-item>
-              <el-menu-item index="2">
+            <el-menu-item index="2" class="eit">
               <i class="el-icon-menu"></i>
               <span slot="title">用户管理</span>
             </el-menu-item>
           </el-menu>
         </el-col>
       </el-row>
+
+      <div class="rightPanel">
+        <OrderInfo />
+      </div>
     </div>
   </div>
 </template>
 <script>
+import OrderInfo from "@/pages/admin/adminHome/orderInfo";
 export default {
   methods: {
     handleOpen(key, keyPath) {
@@ -45,11 +46,13 @@ export default {
       console.log(key, keyPath);
     },
   },
+  components: {
+    OrderInfo,
+  },
 
   mounted() {
     this.$router.push("/adminHome");
   },
-  
 };
 </script>
 
@@ -57,13 +60,21 @@ export default {
 .adminHome {
   header {
     padding-left: 10px;
-    color:white;
+    color: white;
     height: 46px;
     background-color: #545c64;
-    line-height:46px;
+    line-height: 46px;
   }
-  .tac{
-    width: 400px;
+  .tac {
+    float: left;
+    width: 200px;
+    .eit{
+      width: 200px;
+    }
+  }
+  .rightPanel {
+    float: left;
+    
   }
 }
 </style>
