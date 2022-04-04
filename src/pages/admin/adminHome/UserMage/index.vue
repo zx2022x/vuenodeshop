f1f3bc03a245ec70efeaa75b6d84bcb3c8baebfe
13725495536 <1808176136qq@.com>
2022-04-05 00:09:39 +0800

    已经处理好时间格式问题了

--- src/pages/admin/adminHome/UserMage/index.vue
+++ src/pages/admin/adminHome/UserMage/index.vue
@@ -1,43 +1,24 @@
 <template>
-<div>
-  <el-table
-    :data="getUserList"
-    stripe
-    style="width: 100%">
-    <el-table-column
-      prop="id"
-      label="ID"
-      width="50">
-    </el-table-column>
-    <el-table-column
-      prop="user_name"
-      label="用户名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="password"
-      label="密码"
-       width="300"
-      
-      >
-    </el-table-column>
- 
+  <div>
+    <el-table :data="getUserList" stripe style="width: 100%">
+      <el-table-column prop="id" label="ID" width="50"> </el-table-column>
+      <el-table-column prop="user_name" label="用户名" width="180">
+      </el-table-column>
+      <el-table-column prop="password" label="密码" width="300">
+      </el-table-column>
 
-     <el-table-column
-      prop="createdAt"
-      label="注册时间"
-      width="250">
-       <!-- <template slot-scope="scope">
-         {{}}
-       </template> -->
-    </el-table-column>
-    
-     <el-table-column
-      prop="updatedAt"
-      label="更新时间"
-      width="250">
-    </el-table-column>
-     <el-table-column align="left" label="操作" width="100">
+      <el-table-column label="注册时间" width="250">
+        <template slot-scope="scope">
+          <span>{{ handelReTime(scope.$index, scope.row) }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="updatedAt" label="更新时间" width="250">
+        <template slot-scope="scope">
+          <span>{{ handelUaTime(scope.$index, scope.row) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="left" label="操作" width="100">
         <template slot-scope="scope">
           <el-button
             size="mini"
@@ -46,56 +27,67 @@
             >修改密码</el-button
           >
         </template>
-      </el-table-column> 
-  </el-table>
-   <div class="pa">
-    <Pagination :total="getUser.total" @changePnum='changePnum' :pageSize="8"/>
+      </el-table-column>
+    </el-table>
+    <div class="pa">
+      <Pagination
+        :total="getUser.total"
+        @changePnum="changePnum"
+        :pageSize="8"
+      />
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-
-import {mapGetters} from 'vuex'
-import Pagination from "@/pages/admin/Pagination"
-  export default {
-
-
-    data() {
-      return {
-      
-      }
+import { mapGetters } from "vuex";
+import Pagination from "@/pages/admin/Pagination";
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    this.getdata();
+  },
+  methods: {
+    getdata(pageNum = 1, pageSize = 8) {
+      this.$store.dispatch("getUserList", { pageNum, pageSize });
     },
-    mounted(){
-       this.getdata();
+
+    handleChangepw(index, row) {
+      const { id, user_name } = row;
+
+      this.$router.push({
+        name: "changepw",
+        params: { id, user_name, mark: 0 },
+      });
     },
-    methods:{
 
-      getdata(pageNum=1,pageSize=8){
-
-        this.$store.dispatch('getUserList',{pageNum,pageSize})
-         
-      },
-
-       handleChangepw(index,row){
-        const {id,user_name} =row
-        
-        this.$router.push({name:'changepw',params:{id,user_name,mark:0}})
-
-       },
-
-        changePnum(val){
-       
-        this.getdata(val)
-    }
+    changePnum(val) {
+      this.getdata(val);
     },
-    computed:{
-        ...mapGetters(['getUser','getUserList'])
+    //处理注册时间
+    handelReTime(index, row) {
+      const { createdAt } = row;
+      const regTime = createdAt.replace("T", " ").replace(".000Z", "");
+
+      return regTime;
     },
-    components:{
-         Pagination
-    }
-  }
+    //处理更新时间
+    handelUaTime(index, row) {
+      const { updatedAt } = row;
+      const upTime = updatedAt.replace("T", " ").replace(".000Z", "");
+
+      return upTime;
+    },
+  },
+  computed: {
+    ...mapGetters(["getUser", "getUserList"]),
+  },
+  components: {
+    Pagination,
+  },
+};
 </script>
 <style scoped lang='less'>
  .pa {
